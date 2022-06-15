55d849bde03a4df4f40b64af37f2bcb92e08efec
mavas <10604882+mavas@users.noreply.github.com>
2022-06-14 18:43:07 -0700

    Implement server

--- server/server.go
+++ server/server.go
@@ -7,7 +7,9 @@ import (
     "bufio"
     "strings"
     "strconv"
-    //"math/rand"
+    "math/rand"
+    "os"
+    "time"
 )
 
 
@@ -35,4 +37,27 @@ func handleConnection(c net.Conn) {
 
 func main() {
     fmt.Printf("Starting sirkel server on port 8009")
+    arguments := os.Args
+    if len(arguments) == 1 {
+        fmt.Println("Please provide a port number!")
+        return
+    }
+
+    PORT := ":" + arguments[1]
+    l, err := net.Listen("tcp4", PORT)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer l.Close()
+    rand.Seed(time.Now().Unix())
+
+    for {
+        c, err := l.Accept()
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        go handleConnection(c)
+    }
 }
